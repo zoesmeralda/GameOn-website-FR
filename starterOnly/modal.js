d7fc7ada6d4d861e0d6b4035146406ab13b25ada
Zoe esmeralda Carpentier <zoz78@hotmail.fr>
2022-04-27 18:40:02 +0200

    commit sur master

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -11,29 +11,35 @@ function editNav() {
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
-const submitForm = document.querySelectorAll(".btn-submit");
-
+const submitForm = document.querySelector(".btn-submit");
+const closeBtn = document.querySelector(".close");
 //Regex
 //const birthDateRegex = ^(?:0[1-9]|[12]\d|3[01])([\/.-])(?:0[1-9]|1[012])\1(?:19|20)\d\d$;
 const mailRegex = /^\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$/;
 const nameRegex = /(.*[a-z]){2,30}/;
-if (nameRegex) {
-  // utiliser les données ici
-  } else {
+if (nameRegex) { nameRegex = true;
+  
+  
+  } else { nameRegex = false;
   alert("Veuillez entrer 2 caractères ou plus pour le champ du nom.");
   }
-  if (birthDateRegex) {
-    // utiliser les données ici
-    } else {
+  if (birthDateRegex) { birthDateRegex = true;
+    
+    } else { birthDateRegex = false;
     alert("Vous devez entrer votre date de naissance.");
     }
-    if (mailRegex) {
-      // utiliser les données ici
-      } else {
+    if (mailRegex) { mailRegex = true;
+    
+      } else { mailRegex = false;
       alert("Vous devez entrer une adresse email correcte.");
       }
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
+//close modal
+closeBtn.addEventListener("click", closeModal);
+// close modal function
+function closeModal() {
+  modalbg.style.display = "none";
 //confirm submitForm
 submitForm.forEach((btn) => btn.addEventListener("click", confirmSubmit));
 // launch modal form
@@ -45,4 +51,4 @@ function confirmSubmit () {
   alert("Merci ! Votre réservation a été reçue.");
 }
 
-
+}
