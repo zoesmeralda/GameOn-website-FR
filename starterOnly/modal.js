00d43ffe4de0ec454c7dfc9a49a82fd945da5659
Zoe esmeralda Carpentier <zoz78@hotmail.fr>
2022-05-03 15:09:05 +0200

    commit sur master

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -11,44 +11,45 @@ function editNav() {
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
+const radioBtn = document.querySelectorAll(".checkbox-input");
 const submitForm = document.querySelector(".btn-submit");
 const closeBtn = document.querySelector(".close");
 //Regex
-//const birthDateRegex = ^(?:0[1-9]|[12]\d|3[01])([\/.-])(?:0[1-9]|1[012])\1(?:19|20)\d\d$;
+
 const mailRegex = /^\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$/;
 const nameRegex = /(.*[a-z]){2,30}/;
-if (nameRegex) { nameRegex = true;
-  
-  
-  } else { nameRegex = false;
-  alert("Veuillez entrer 2 caractères ou plus pour le champ du nom.");
-  }
-  if (birthDateRegex) { birthDateRegex = true;
-    
-    } else { birthDateRegex = false;
-    alert("Vous devez entrer votre date de naissance.");
-    }
-    if (mailRegex) { mailRegex = true;
-    
-      } else { mailRegex = false;
-      alert("Vous devez entrer une adresse email correcte.");
-      }
+
+const matchRegex = formData.match(mailRegex,nameRegex);
+
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
-//close modal
-closeBtn.addEventListener("click", closeModal);
-// close modal function
-function closeModal() {
-  modalbg.style.display = "none";
-//confirm submitForm
-submitForm.forEach((btn) => btn.addEventListener("click", confirmSubmit));
 // launch modal form
 function launchModal() {
   modalbg.style.display = "block";
 }
+//verif bouton radio
+
+function radio(radioBtn) {
+  let radioBtn = 0;
+  let messageAlert = window.alert("veuillez selectionner au moins 1 bouton");
+  for (let i = 0; i < radioBtnn.options.length; i++) {
+    if (radioBtn = 0)
+  }
+  return messageAlert;
+}
+//close modal
+closeBtn.addEventListener("click", closeModal);
+//confirm submitForm
+submitForm.addEventListener("click", confirmSubmit);
+
+// close modal function
+function closeModal() {
+  modalbg.style.display = "none";
+}
 //confirmation d'envoi du formulaire
 function confirmSubmit () {
-  alert("Merci ! Votre réservation a été reçue.");
+  console.log("confirmSubmit");
+  window.alert("Merci ! Votre réservation a été reçue.");
 }
 
-}
+
