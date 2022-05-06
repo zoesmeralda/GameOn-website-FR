df9f9fb03e73fa864ffeea8492372a02368c1e6f
Zoe esmeralda Carpentier <zoz78@hotmail.fr>
2022-05-06 12:14:40 +0200

    commit sur master

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -17,7 +17,14 @@ const closeBtn = document.querySelector(".close");
 const email = document.getElementById("email");
 const firstName = document.getElementById("first");
 const lastName = document.getElementById("last");
-
+const dob = document.getElementById("birthdate");
+const checkbox1 = document.getElementById("location1");
+const checkbox2 = document.getElementById("location2");
+const checkbox3 = document.getElementById("location3");
+const checkbox4 = document.getElementById("location4");
+const checkbox5 = document.getElementById("location5");
+const checkbox6 = document.getElementById("location6");
+const checkboxCgu = document.getElementById("checkbox2-label");
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
 // launch modal form
@@ -44,13 +51,42 @@ function lastVerification() {
 	if (inputValue !== null && inputValue.length >= 2) return true;
 	else return false;
 }
+
+function validBirthDate() {
+  result = true;
+    
+  var userBirthDate = new Date(document.getElementById("birthdate").value);
+
+  var regexVar = new RegExp("([0-9]{2})\/([0-9]{2})\/([0-9]{4})");
+  var regexVarTest = regexVar.test(userBirthDate);
+
+
+  if (isNaN(userBirthDate)) {
+    dobErrMsg.innerHTML = "la date de naissance doit être un chiffre";
+  } else if (regexVarTest == false) {
+    dobErrMsg.innerHTML = "entrer la date au format jj/mm/aaaa";
+  } 
+
+  return result;
+
+}
 //verif bouton radio
 
-function radio(radioB) {
-  let radioBn = 0;
-  let messageAlert = window.alert("veuillez selectionner au moins 1 bouton");
-  for (let i = 0; i < radioBn.options.length; i++) {
-    if (radioB > 1)
+function radio(radioBtn) {
+  
+  let messageAlert = window.alert("veuillez selectionner au moins 1 option");
+  for (let i = 0; i < radioBtn.options.length; i++) {
+    if (radioBtn.checked=false)
+  
+  
+  return messageAlert;
+}}
+//verif condition d'utilisation
+function CGU(checkboxCgu) {
+  
+  let messageAlert = window.alert("veuillez cocher la case pour accepter les CGU");
+  for (let i = 0; i < checkboxCgu.options.length; i++) {
+    if (checkboxCgu.checked=false)
   
   
   return messageAlert;
