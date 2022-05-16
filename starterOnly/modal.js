750ae1b0d95bc160cce6bddffbf8295774174500
Zoe esmeralda Carpentier <zoz78@hotmail.fr>
2022-05-16 12:29:50 +0200

    commit sur master

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -19,7 +19,6 @@ const email = document.getElementById("email");
 const firstName = document.getElementById("first");
 const lastName = document.getElementById("last");
 
-
 let rightFirstName;
 let rightLastName;
 let rightEmail;
@@ -32,8 +31,7 @@ let CGUChecked;
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
 // launch modal form
 function launchModal() {
-  modalbg.style.display = "block"
-
+  modalbg.style.display = "block";
 }
 //close modal
 closeBtn.addEventListener("click", closeModal);
@@ -44,39 +42,51 @@ function closeModal() {
 }
 //Verification email
 
-
 function emailVerification(myEmail) {
+  console.log("email check");
   if (myEmail.match(/^[a-zA-Z0-9_+\.]+@[a-zA-Z0-9]+\.[a-z]+$/)) {
     rightEmail = true;
     document.getElementById("formData3").setAttribute("data-error", "false");
-    document.getElementById("formData3").setAttribute("data-error-visible", "false");
+    document
+      .getElementById("formData3")
+      .setAttribute("data-error-visible", "false");
   } else {
     rightEmail = false;
-    document.getElementById("formData3").setAttribute("data-error", "Veuillez saisir une adresse e-mail valide");
-    document.getElementById("formData3").setAttribute("data-error-visible", "true");
+    document
+      .getElementById("formData3")
+      .setAttribute("data-error", "Veuillez saisir une adresse e-mail valide");
+    document
+      .getElementById("formData3")
+      .setAttribute("data-error-visible", "true");
   }
-
 }
 
 /*function emailVerification() {
 	const mailRegex = /^\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$/;
 	return mailRegex.test(email.value);}
 */
-  
+
 // Verification firstname
 
-
 function firstVerification(firstName) {
-  if ((firstName.length >= 2) && (!firstName.match(/^[\d]+$/))) {
+  rightFirstName = false;
+  if (firstName.length >= 2 && !firstName.match(/^[\d]+$/)) {
     rightFirstName = true;
     document.getElementById("formData1").setAttribute("data-error", "false");
-    document.getElementById("formData1").setAttribute("data-error-visible", "false");
+    document
+      .getElementById("formData1")
+      .setAttribute("data-error-visible", "false");
   } else {
     rightFirstName = false;
-    document.getElementById("formData1").setAttribute("data-error", "Veuillez saisir un prénom valide");
-    document.getElementById("formData1").setAttribute("data-error-visible", "true");
+    document
+      .getElementById("formData1")
+      .setAttribute("data-error", "Veuillez saisir un prénom valide");
+    document
+      .getElementById("formData1")
+      .setAttribute("data-error-visible", "true");
   }
-
+  // TODO il faut que tu fasse ce return sur les autres fonctions aussi
+  return rightFirstName;
 }
 /*function firstVerification() {
 	let inputValue = firstName.value;
@@ -87,16 +97,22 @@ function firstVerification(firstName) {
 // Verification lastname
 
 function lastVerification(lastName) {
-  if ((lastName.length >= 2) && (!lastName.match(/^[\d]+$/))) {
+  if (lastName.length >= 2 && !lastName.match(/^[\d]+$/)) {
     rightLastName = true;
     document.getElementById("formData2").setAttribute("data-error", "false");
-    document.getElementById("formData2").setAttribute("data-error-visible", "false");
+    document
+      .getElementById("formData2")
+      .setAttribute("data-error-visible", "false");
   } else {
     rightLastName = false;
-    document.getElementById("formData2").setAttribute("data-error", "Veuillez saisir un nom valide");
-    document.getElementById("formData2").setAttribute("data-error-visible", "true");
+    document
+      .getElementById("formData2")
+      .setAttribute("data-error", "Veuillez saisir un nom valide");
+    document
+      .getElementById("formData2")
+      .setAttribute("data-error-visible", "true");
   }
-
+  return rightLastName;
 }
 /*function lastVerification() {
 	let inputValue = lastName.value;
@@ -105,20 +121,27 @@ function lastVerification(lastName) {
 }*/
 
 function birthdateVerification(myDate) {
-
   const todaysDate = new Date();
   const myBirthDate = Date.parse(myDate);
   if (todaysDate > myBirthDate) {
     rightDate = true;
     document.getElementById("formData4").setAttribute("data-error", "false");
-    document.getElementById("formData4").setAttribute("data-error-visible", "false");
+    document
+      .getElementById("formData4")
+      .setAttribute("data-error-visible", "false");
   } else {
     rightDate = false;
-    document.getElementById("formData4").setAttribute("data-error", "Veuillez saisir une date de naissance valide");
-    document.getElementById("formData4").setAttribute("data-error-visible", "true");
+    document
+      .getElementById("formData4")
+      .setAttribute(
+        "data-error",
+        "Veuillez saisir une date de naissance valide"
+      );
+    document
+      .getElementById("formData4")
+      .setAttribute("data-error-visible", "true");
   }
-
-
+  return rightDate;
 }
 /*function validBirthDate() {
   result = true;
@@ -139,42 +162,61 @@ function birthdateVerification(myDate) {
 
 }*/
 
-
 function numberVerification(myNumber) {
   if (myNumber === "") {
     rightNumber = false;
-    document.getElementById("formData5").setAttribute("data-error", "Veuillez saisir un nombre");
-    document.getElementById("formData5").setAttribute("data-error-visible", "true");
+    document
+      .getElementById("formData5")
+      .setAttribute("data-error", "Veuillez saisir un nombre");
+    document
+      .getElementById("formData5")
+      .setAttribute("data-error-visible", "true");
   } else {
-    if (((Number(myNumber) > -1) && (Number(myNumber) < 100))) {
+    if (Number(myNumber) > -1 && Number(myNumber) < 100) {
       rightNumber = true;
       document.getElementById("formData5").setAttribute("data-error", "false");
-      document.getElementById("formData5").setAttribute("data-error-visible", "false");
+      document
+        .getElementById("formData5")
+        .setAttribute("data-error-visible", "false");
     } else {
       rightNumber = false;
-      document.getElementById("formData5").setAttribute("data-error", "Veuillez saisir un nombre");
-      document.getElementById("formData5").setAttribute("data-error-visible", "true");
+      document
+        .getElementById("formData5")
+        .setAttribute("data-error", "Veuillez saisir un nombre");
+      document
+        .getElementById("formData5")
+        .setAttribute("data-error-visible", "true");
     }
   }
-
+  return rightNumber;
 }
-
 
 //verif bouton radio
 
-
 function radio() {
-  if ((location1.checked) || (location2.checked) || (location3.checked) || (location4.checked) ||
-    (location5.checked) || (location6.checked)) {
+  if (
+    location1.checked ||
+    location2.checked ||
+    location3.checked ||
+    location4.checked ||
+    location5.checked ||
+    location6.checked
+  ) {
     radioChecked = true;
     document.getElementById("formData6").setAttribute("data-error", "false");
-    document.getElementById("formData6").setAttribute("data-error-visible", "false");
+    document
+      .getElementById("formData6")
+      .setAttribute("data-error-visible", "false");
   } else {
     radioChecked = false;
-    document.getElementById("formData6").setAttribute("data-error", "veuillez selectionner au moins 1 option");
-    document.getElementById("formData6").setAttribute("data-error-visible", "true");
+    document
+      .getElementById("formData6")
+      .setAttribute("data-error", "veuillez selectionner au moins 1 option");
+    document
+      .getElementById("formData6")
+      .setAttribute("data-error-visible", "true");
   }
-
+  return radioChecked;
 }
 
 /*function radio(radioBtn) {
@@ -192,13 +234,22 @@ function CGU() {
   if (checkbox1.checked) {
     CGUChecked = true;
     document.getElementById("formData7").setAttribute("data-error", "false");
-    document.getElementById("formData7").setAttribute("data-error-visible", "false");
+    document
+      .getElementById("formData7")
+      .setAttribute("data-error-visible", "false");
   } else {
     CGUChecked = false;
-    document.getElementById("formData7").setAttribute("data-error", "veuillez cocher la case pour accepter les CGU");
-    document.getElementById("formData7").setAttribute("data-error-visible", "true");
+    document
+      .getElementById("formData7")
+      .setAttribute(
+        "data-error",
+        "veuillez cocher la case pour accepter les CGU"
+      );
+    document
+      .getElementById("formData7")
+      .setAttribute("data-error-visible", "true");
   }
-
+  return CGUChecked;
 }
 /*function CGU(checkboxCgu) {
   
@@ -210,6 +261,9 @@ function CGU() {
   return messageAlert;
 }}*/
 
+submitForm.addEventListener("click", (e) => {
+  confirmSubmit(e);
+});
 
 function confirmSubmit(e) {
   e.preventDefault();
@@ -226,26 +280,21 @@ function confirmSubmit(e) {
   const radioData = document.querySelectorAll(".checkbox-input").value;
   radio();
   const CGUData = document.querySelectorAll(".checkbox-input").value;
-  CGU()
+  CGU();
 
-//confirm submitForm
-submitForm.addEventListener("click", confirmSubmit);
-console.log("sub");
+  //confirmation d'envoi du formulaire si conditions remplies et affichage du block de confirmation d'inscription
 
-
-//confirmation d'envoi du formulaire si conditions remplies et affichage du block de confirmation d'inscription
-
-
-// if ((firstVerification) && (lastVerification) && (emailVerification) &&
-// (birthdateVerification) && (numberVerification) && (radio) && (CGU)) {
-
-modalbg2.style.display = "block";
-// }
-
+  // TODO: faire pareil pour les autres fonctions appelées dans le if
+  // Regarde dans la fonction firstVerification, j'ai ajouté un return de la valeur (sinon ton if sera toujours vrai)
+  if (
+    firstVerification(firstNameData) &&
+    lastVerification &&
+    emailVerification &&
+    birthdateVerification &&
+    numberVerification &&
+    radio &&
+    CGU
+  ) {
+    modalbg2.style.display = "block";
+  }
 }
-/*function confirmSubmit () {
-  console.log("confirmSubmit");
-  window.alert("Merci ! Votre réservation a été reçue.");
-}*/
-
-
